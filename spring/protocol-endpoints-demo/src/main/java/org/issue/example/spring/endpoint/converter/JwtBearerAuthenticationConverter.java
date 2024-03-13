7ead6e25cf62820de1c456b0c158f7eba5c34f10
zll_space <zll_space@163.com>
2024-03-13 16:05:27 +0800

    修改

--- spring/protocol-endpoints-demo/src/main/java/org/issue/example/spring/endpoint/converter/JwtBearerAuthenticationConverter.java
+++ spring/protocol-endpoints-demo/src/main/java/org/issue/example/spring/endpoint/converter/JwtBearerAuthenticationConverter.java
@@ -2,18 +2,13 @@ package org.issue.example.spring.endpoint.converter;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.crypto.impl.RSASSA;
+import com.nimbusds.jose.jca.JCAContext;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jose.util.Base64;
 import com.nimbusds.jose.util.Base64URL;
-import com.nimbusds.jose.util.JSONObjectUtils;
-import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,8 +19,6 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
-import org.springframework.security.oauth2.jose.jws.JwsAlgorithm;
-import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationException;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -36,19 +29,18 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.*;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
 import java.text.ParseException;
 import java.util.*;
 
 /**
- * @Author: Mr.Zhao
- * @Description:
- * @Date:Create：in 2024/3/7 15:50
- * @Modified By:
+ * 自定义Using JWTs as Authorization Grants。
+ * <a href="https://blog.csdn.net/qq_31772441/article/details/127099119">参考private_key_jwt</a>
+ * <a href="https://datatracker.ietf.org/doc/html/rfc7523#section-2.1">参考Using JWTs as Authorization Grants</a>
+ * 参考{@link com.nimbusds.jose.proc.JWSAlgorithmFamilyJWSKeySelector}
  */
 public class JwtBearerAuthenticationConverter implements AuthenticationConverter {
     private static final String DEFAULT_ERROR_URI = "https://datatracker.ietf.org/doc/html/rfc7523#section-2.1";
@@ -57,11 +49,10 @@ public class JwtBearerAuthenticationConverter implements AuthenticationConverter
     private final RegisteredClientRepository registeredClientRepository;
     private HttpSecurity httpSecurity;
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-    private JWKSource<SecurityContext> jwkSource;
+    private final String JWK_SET_URL = "jwk-set-url";
 
     public JwtBearerAuthenticationConverter(HttpSecurity httpSecurity) {
         this.httpSecurity = httpSecurity;
-        this.jwkSource = httpSecurity.getSharedObject(JWKSource.class);
         this.registeredClientRepository = httpSecurity.getSharedObject(RegisteredClientRepository.class);
     }
 
@@ -81,6 +72,56 @@ public class JwtBearerAuthenticationConverter implements AuthenticationConverter
             throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
         }
 
+        String[] parts = assertion.split("\\.");
+        if (3 != parts.length) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
+        }
+
+        Base64URL headerBase64 = Base64URL.from(parts[0]);
+        Base64URL payloadBase64 = Base64URL.from(parts[1]);
+        Base64URL signatureBase64 = Base64URL.from(parts[2]);
+
+        JWSHeader jwsHeader = null;
+        try {
+            jwsHeader = JWSHeader.parse(headerBase64);
+        } catch (ParseException e) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
+        }
+        JWSAlgorithm jwsAlgorithm = jwsHeader.getAlgorithm();
+
+        JWTClaimsSet jwtClaimsSet = null;
+        try {
+            jwtClaimsSet = JWTClaimsSet.parse(payloadBase64.decodeToString());
+        } catch (ParseException e) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
+        }
+
+        String Subject = jwtClaimsSet.getSubject();
+        RegisteredClient registeredClient = registeredClientRepository.findByClientId(Subject);
+        if (null == registeredClient) {
+            throwError(OAuth2ErrorCodes.INVALID_CLIENT, OAuth2ParameterNames.ASSERTION);
+        }
+
+        Map<String, Object> claims = jwtClaimsSet.getClaims();
+        if (null == claims || claims.isEmpty()) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
+        }
+
+        String jwkSetUrl = (String) claims.get(JWK_SET_URL);
+        if (!StringUtils.hasText(jwkSetUrl)) {
+            jwkSetUrl = registeredClient.getClientSettings().getJwkSetUrl();
+            if (!StringUtils.hasText(jwkSetUrl)) {
+                throwError(OAuth2ErrorCodes.INVALID_REQUEST, JWK_SET_URL);
+            }
+        }
+
+        URL url = null;
+        try {
+            url = new URL(jwkSetUrl);
+        } catch (MalformedURLException e) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
+        }
+
         // scope (OPTIONAL)
         Set<String> scopes = null;
         String scope = parameters.getFirst(OAuth2ParameterNames.SCOPE);
@@ -93,108 +134,38 @@ public class JwtBearerAuthenticationConverter implements AuthenticationConverter
                     Arrays.asList(StringUtils.delimitedListToStringArray(scope, " ")));
         }
 
-        String[] parts = assertion.split("\\.");
-        if (3 != parts.length) {
-            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
-        }
+        int length = assertion.lastIndexOf("\\.");
+        Base64URL signedContent = Base64URL.from(assertion.substring(length + 1));
 
-        Base64 headerBase64 = new Base64(parts[0]);
-        Base64 payloadBase64 = new Base64(parts[1]);
-        Base64 signatureBase = new Base64(parts[2]);
 
-        String headerJson = headerBase64.decodeToString();
-        String payload = payloadBase64.decodeToString();
-
-        Map<String, Object> claims = null;
-        Map<String, Object> header = null;
+        JWKSet jwkSet = null;
         try {
-            claims = JSONObjectUtils.parse(payload);
-            header = JSONObjectUtils.parse(headerJson);
+            jwkSet = JWKSet.load(url);
+        } catch (IOException e) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
         } catch (ParseException e) {
             throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
         }
 
-        String clientId = (String) claims.get("sub");
-        /*
-        RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
-        if (registeredClient == null) {
-            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
-        }
+        JWK jwk = jwkSet.getKeys().get(0);
 
-        JwsAlgorithm jwsAlgorithm = registeredClient.getClientSettings().getTokenEndpointAuthenticationSigningAlgorithm();
-        JwsHeader jwsHeader = JwsHeader.with(jwsAlgorithm).build();
-
-        List<String> audience = new ArrayList<>();
-        audience.add(registeredClient.getClientId());
-
-        Instant issuedAt = Instant.now();
-        // 构造参考：JwtClientAssertionDecoderFactory#defaultJwtValidatorFactory方法
-        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-                .issuer(registeredClient.getClientId())
-                .subject(registeredClient.getClientId())
-                .audience(audience)
-                .issuedAt(issuedAt)// 访问时间
-                .expiresAt(issuedAt.plus(registeredClient.getTokenSettings().getAccessTokenTimeToLive()))// 过期时间
-                .issuer(registeredClient.getClientSettings().getJwkSetUrl())
-                .claim(OAuth2ParameterNames.SCOPE, registeredClient.getScopes())
-                .build();
-
-
-        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(jwsHeader, jwtClaimsSet);
-
-        NimbusJwtEncoder nimbusJwtEncoder =  new NimbusJwtEncoder(jwkSource);
-        Jwt jwtAssertion = nimbusJwtEncoder.encode(jwtEncoderParameters);
-        */
-        ImmutableJWKSet immutableJWKSet = (ImmutableJWKSet)jwkSource;
-        JWKSet jwkSet = immutableJWKSet.getJWKSet();
-        JWK jwk= jwkSet.getKeys().get(0);
-
-        Base64URL privateExponentBase64 =((RSAKey) jwk).getPrivateExponent();
-        Base64URL modulusBase64 =((RSAKey) jwk).getModulus();
-
-        // e、n的其实是publicExponent和modulus的base64格式
-        // 正常私钥签名，公钥验证。现在是客户端用公钥签名，服务端用私钥验证
-        BigInteger publicExponent = new BigInteger(1, privateExponentBase64.decode());
-        BigInteger modulus = new BigInteger(1, modulusBase64.decode());
-
-        RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(modulus, publicExponent);
-
-        KeyFactory keyFactory = null;
+        Signature signature;
+        boolean result = false;
         try {
-            keyFactory = KeyFactory.getInstance("RSA");
-        } catch (NoSuchAlgorithmException e) {
+            signature = RSASSA.getSignerAndVerifier(jwsAlgorithm, new JCAContext().getProvider());
+            signature.initVerify(jwk.toPublicJWK().toRSAKey().toRSAPublicKey());
+            signature.update(signedContent.decode());
+            result = signature.verify(signatureBase64.decode());
+        } catch (JOSEException e) {
             e.printStackTrace();
-        }
-
-        PublicKey publicKey = null;
-        try {
-            publicKey = keyFactory.generatePublic(rsaPublicKeySpec);
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
-
-
-        Signature verifier = null;
-        try {
-            verifier = Signature.getInstance("SHA256withRSA");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            verifier.initVerify(publicKey);
-
         } catch (InvalidKeyException e) {
-
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
         }
 
-        boolean result;
-
-        try {
-            verifier.update((headerBase64+"."+headerBase64).getBytes(StandardCharsets.UTF_8));
-            result = verifier.verify(signatureBase.decode());
-        } catch (SignatureException e) {
-
+        if (!result) {
+            throwError(OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ParameterNames.ASSERTION);
         }
 
         Map<String, Object> additionalParameters = new HashMap<>();
@@ -206,7 +177,7 @@ public class JwtBearerAuthenticationConverter implements AuthenticationConverter
             }
         });
 
-        return new JwtBearerAuthenticationToken(null, assertion, scopes, ClientAuthenticationMethod.PRIVATE_KEY_JWT,
+        return new JwtBearerAuthenticationToken(registeredClient, assertion, scopes, ClientAuthenticationMethod.PRIVATE_KEY_JWT,
                 additionalParameters);
     }
 

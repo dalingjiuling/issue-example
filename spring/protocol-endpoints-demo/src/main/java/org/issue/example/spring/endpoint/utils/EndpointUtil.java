23635984e556ae68bc0832daef27f572d55a40ce
zll_space <zll_space@163.com>
2024-03-15 17:25:55 +0800

    删除方法

--- spring/protocol-endpoints-demo/src/main/java/org/issue/example/spring/endpoint/utils/EndpointUtil.java
+++ spring/protocol-endpoints-demo/src/main/java/org/issue/example/spring/endpoint/utils/EndpointUtil.java
@@ -354,23 +354,4 @@ public class EndpointUtil {
     public static String getOpenidLogoutUrl(String token) {
         return null;
     }
-
-    public static String usingJWTsAsAuthorizationGrants(String jwt) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("fetch(\"http://127.0.0.1:6004/oauth2/token\", {");
-        stringBuilder.append("\"headers\": {");
-        stringBuilder.append("\"content-type\": \"application/x-www-form-urlencoded; charset=UTF-8\",");
-        stringBuilder.append("},");
-        stringBuilder.append("\"method\": \"POST\",");
-        stringBuilder.append("\"body\":\"");
-        stringBuilder.append("grant_type=");
-        stringBuilder.append(URLEncoder.encode("urn:ietf:params:oauth:grant-type:jwt-bearer", StandardCharsets.UTF_8));
-        stringBuilder.append("&");
-        stringBuilder.append("assertion=");
-        stringBuilder.append(URLEncoder.encode(jwt, StandardCharsets.UTF_8));
-
-        stringBuilder.append("\"");
-        stringBuilder.append("}).then(json=>console.log(json));");
-        return stringBuilder.toString();
-    }
 }
